20de2c44f285a7ed98c1b53eb8631e51e7f46bd8
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2023-02-13 17:18:56 -0800

    add to gitignore, format `gcutil.h`

--- gc/gcutil.h
+++ gc/gcutil.h
@@ -2,19 +2,23 @@
 #define __GCUTIL_H__
 
 #ifndef ATTRIBUTE_ALIGN
-# define ATTRIBUTE_ALIGN(v)					__attribute__((aligned(v)))
+# define ATTRIBUTE_ALIGN(v)	__attribute__((aligned(v)))
 #endif
+
 #ifndef ATTRIBUTE_PACKED
-# define ATTRIBUTE_PACKED					__attribute__((packed))
+# define ATTRIBUTE_PACKED	__attribute__((packed))
 #endif
 
+// mask the first w bits of v before lshifting by s bits
 #define _SHIFTL(v, s, w)	\
-    ((u32) (((u32)(v) & ((0x01 << (w)) - 1)) << (s))) //mask the first w bits of v before lshifting
-#define _SHIFTR(v, s, w)	\
-    ((u32)(((u32)(v) >> (s)) & ((0x01 << (w)) - 1)))  //rshift v and mask the first w bits afterwards
+	((u32) (((u32)(v) & ((0x01 << (w)) - 1)) << (s)))
 
+// rshift v by s bits and mask the first w bits afterwards
+#define _SHIFTR(v, s, w)	\
+	((u32)(((u32)(v) >> (s)) & ((0x01 << (w)) - 1)))
+
+// lrotate v by s bits
 #define _ROTL(v,s)			\
 	(((u32)v<<s)|((u32)v>>(0x20-s)))
 
 #endif /* _GCUTIL_H */
-
