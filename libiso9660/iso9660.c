d69cbdcc1333a2dcb50c13ee6157f30e73f9629b
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2023-04-15 22:30:47 -0700

    `iso9660.h`: remove `ogc/lwp_watchdog.h` and `ogcsys.h`, add `MAX` and `MIN` macros
    fun fact: originally this file used the `MAX` and `MIN` macros from `ogc/gu.h`

--- libiso9660/iso9660.c
+++ libiso9660/iso9660.c
@@ -6,8 +6,6 @@
  ****************************************************************************/
 
 #include <errno.h>
-#include <ogc/lwp_watchdog.h>
-#include <ogcsys.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,6 +27,14 @@
 #define DIR_SEPARATOR		'/'
 
 #define FLAG_DIR 2
+
+#ifndef MAX
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#endif
+
+#ifndef MIN
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#endif
 
 struct pvd_s
 {
